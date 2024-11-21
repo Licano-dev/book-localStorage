9a80d4d23b5ad53e51012cd74ac79ebf6fc88b0d
yonier999 <yonierjulian999@gmail.com>
2024-11-21 13:02:54 -0500

    url de redireción actualizada

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,5 @@
 const close = () => {
-  window.location.href = "/index.html";
+  window.location.href = "https://licano-dev.github.io/book-localStorage/";
 };
 
 const limpiarAlerta = (elemento) => {
@@ -25,7 +25,7 @@ const alertaExito = (texto) => {
   });
   setTimeout(function () {
     //despues de 2 seg ejecurar la redirección
-    window.location.href = "/index.html";
+    window.location.href = "https://licano-dev.github.io/book-localStorage/";
   }, 2000);
 };
 const limpirarListaContenidoListado = (parentNode) => {
